350fd1bffb648b35951d4a6c983adcc58810995e
Benjamin Williams <benjamin@opensystemlabs.io>
2023-01-13 14:50:08 +0000

    Update validatePlanXExportData

--- src/overview/helpers.test.ts
+++ src/overview/helpers.test.ts
@@ -1,21 +1,31 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { validatePlanXExportData } from "./helpers";
 
-test("validatePlanXExportData", () => {
-  expect(
-    validatePlanXExportData([
-      {
-        question: "is it?",
-        responses: "yes",
-      },
-    ])
-  ).toBe(true);
-  expect(
-    validatePlanXExportData([
-      {
-        q: "really?",
-        a: "no",
-      },
-    ])
-  ).toBe(false);
+describe("validatePlanXExportData", () => {
+  test("undefined", () => {
+    expect(validatePlanXExportData(undefined)).toBe(false);
+  });
+  test("empty", () => {
+    expect(validatePlanXExportData([])).toBe(false);
+  });
+  test("valid", () => {
+    expect(
+      validatePlanXExportData([
+        {
+          question: "is it?",
+          responses: "yes",
+        },
+      ])
+    ).toBe(true);
+  });
+  test("invalid", () => {
+    expect(
+      validatePlanXExportData([
+        {
+          q: "really?",
+          a: "no",
+        },
+      ])
+    ).toBe(false);
+  });
 });
