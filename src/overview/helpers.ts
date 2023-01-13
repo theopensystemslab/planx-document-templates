8556024a553f6759fbe28b496368b0ea7890edc4
Benjamin Williams <benjamin@opensystemlabs.io>
2023-01-13 14:50:08 +0000

    Update validatePlanXExportData

--- src/overview/helpers.ts
+++ src/overview/helpers.ts
@@ -2,7 +2,8 @@ import type { PlanXExportData } from "../types";
 
 export function validatePlanXExportData(data: PlanXExportData[]): boolean {
   return (
-    data &&
+    Array.isArray(data) &&
+    data.length > 0 &&
     data.every((entry: object) => {
       return (
         Object.hasOwn(entry, "question") && Object.hasOwn(entry, "responses")
