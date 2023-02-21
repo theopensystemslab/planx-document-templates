7eb748a923978ba847713d1665262c96792ecb25
Benjamin Williams <benjamin@opensystemslab.io>
2023-02-21 09:07:49 +0000

    Ensure example dir is created

--- src/templates/generateTemplateExamples.ts
+++ src/templates/generateTemplateExamples.ts
@@ -1,4 +1,9 @@
-import { createWriteStream, writeFileSync } from "node:fs";
+import {
+  createWriteStream,
+  writeFileSync,
+  existsSync,
+  mkdirSync,
+} from "node:fs";
 import { Packer } from "docx";
 import { generateDocxTemplateStream, TEMPLATES } from "../";
 import { buildTestTemplate } from "./testTemplate";
@@ -12,10 +17,18 @@ import exampleLDCData from "../data/exampleLDC.json";
 })();
 
 async function generateTemplateExamples() {
+  // create example directory
+  const exampleDir = "./examples";
+  if (!existsSync(exampleDir)) {
+    mkdirSync(exampleDir);
+  }
+
+  // build test doc
   await Packer.toBuffer(buildTestTemplate()).then((buffer) => {
     writeFileSync(`./examples/Test.docx`, buffer);
   });
 
+  // build templates
   const promises: Promise<void>[] = Object.keys(TEMPLATES).map(
     async (templateName) => {
       const file = createWriteStream(`./examples/${templateName}.docx`);
@@ -30,6 +43,7 @@ async function generateTemplateExamples() {
     }
   );
 
+  // ensure all templates built succesfully otherwise exit with an error
   await Promise.all(promises).catch((e) => {
     console.log(e);
     process.exit(1); // fail the build if an example failed to generate
